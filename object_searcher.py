2702fd99e67d4afbce96aae10562b678d921a338
Shamsiddin Kamoliddinov <160200088+bekfurr@users.noreply.github.com>
2024-10-28 19:32:34 +0500

    object_searcher.py

--- object_searcher.py
+++ object_searcher.py
@@ -2,37 +2,45 @@ import tkinter as tk
 from tkinter import messagebox
 
 def qidirish():
-
-    a = list(map(str,entry_massiv.get().split()))  
+    a = list(map(str, entry_massiv.get().split()))
     m = entry_qidirilayotgan_object.get()
-    
+    hisobchi = 0
+    indeks_xab = ""
+
     for i in range(len(a)):
         if m == a[i]:
-            messagebox.showinfo("Natija", f"Object topildi! Index: {i}")
-            return
-    messagebox.showinfo("Natija", "object topilmadi")
+            if hisobchi == 0:
+                indeks_xab = f"Object topildi! Index:{i}"
+            else:
+                indeks_xab += f", {i}"
+            hisobchi += 1
 
+    if hisobchi > 0:
+        messagebox.showinfo("Natija", f"{indeks_xab}\nObject ro'yxatda {hisobchi} marta foydalanilgan")
+    else:
+        messagebox.showinfo("Natija", "Object topilmadi")
 
-root = tk.Tk()
-root.title("Massivda son qidirish")
-root.geometry("300x200")
+oyna = tk.Tk()
+oyna.title("Massivda son qidirish")
+oyna.geometry("500x200")
 
-
-label_massiv = tk.Label(root, text="Massivni kiriting (probel bilan ajratib):")
+label_massiv = tk.Label(oyna, text="Massivni kiriting (probel bilan ajratib):")
 label_massiv.pack()
 
-entry_massiv = tk.Entry(root, width=65)
+entry_massiv = tk.Entry(oyna, width=85)
 entry_massiv.pack()
 
-label_qidirilayotgan_object = tk.Label(root, text="Qidirilayotgan Objectni kiriting:")
+label_qidirilayotgan_object = tk.Label(oyna, text="Qidirilayotgan Objectni kiriting:")
 label_qidirilayotgan_object.pack()
 
-entry_qidirilayotgan_object = tk.Entry(root, width=30)
+entry_qidirilayotgan_object = tk.Entry(oyna, width=50)
 entry_qidirilayotgan_object.pack()
 
-
-button_qidirish = tk.Button(root, text="Qidirish", command=qidirish)
+button_qidirish = tk.Button(oyna, text="Qidirish", command=qidirish)
 button_qidirish.pack(pady=10)
 
+oyna.mainloop()
 
-root.mainloop()
+# CODE CREATED WITH PYHTON AT 10.27.2024
+# CODER: @BEKFURR
+# Github Repository link:https://github.com/bekfurr/Obeject_qidir
